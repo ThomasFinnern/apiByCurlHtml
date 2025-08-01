ccf5f886a4c2abdc1aa41c9dce70f4bc20c3cb19
ThomasFinnern <ThomasFinnern@users.noreply.github.com>
2025-08-01 21:37:12 +0200

    Created *.http files from *.tsk files

---
+++ src/curl_tasks_tsk/_task2http_allFiles.bat
@@ -0,0 +1,34 @@
+@echo Off
+REM ----------------------------------------
+REM Transform all *.tsk files to *.http file format 
+REM ----------------------------------------
+
+@echo Transform all *.tsk files to *.http file format 
+@echo.
+
+REM Call transformation on each matching file
+REM for /f %%f in ('dir /b *.tsk	') do ( 
+
+for %%i in (*.tsk) do CALL :tsk2httpFile %%i
+
+@ECHO -----------------------------------------------
+@Echo all *.tsk files are transferred
+@ECHO -----------------------------------------------
+REM pause
+
+goto :EOF
+
+REM ----------------------------------------
+REM transform all *.tsk files into *.html file (in other folder)
+REm ----------------------------------------
+
+:tsk2httpFile
+Rem expected files and folders befor deleting .\progs\*.hel ...
+
+ECHO tsk2httpFile: "%1"
+
+REM if not exist %1\nul goto :EOF
+
+CALL _task2http_file.bat %1
+
+goto :EOF
---
+++ src/curl_tasks_tsk/_task2http_file.bat
@@ -0,0 +1,72 @@
+@echo Off
+REM ----------------------------------------
+REM Transform *.tsk files to *.http file format 
+
+@echo Transform all *.tsk files to *.http file format 
+@echo.
+
+REM --- files and folders -------------------------------------------------
+
+
+if "%1" NEQ "" (
+	SET srcFile=%1
+) else (
+	set srcFile=rsg2_getGallery.tsk
+)
+
+if "%2" NEQ "" (
+	SET srcPath=%2
+) else (
+REM	set srcPath=".\"
+REM	set srcPath=.\
+	set srcPath=d:\Entwickl\2025\_gitHub\apiByCurlHtml\src\curl_tasks_tsk
+)
+
+if "%3" NEQ "" (
+	SET dstFile=%3
+) else (
+	set dstFile=%srcFile%.http
+)
+
+if "%4" NEQ "" (
+	SET dstPath=%4
+) else (
+	set dstPath=%srcPath%
+)
+
+ECHO srcFile: %srcFile%
+ECHO srcPath: %srcPath%
+ECHO dstFile: %dstFile%
+ECHO dstPath: %dstPath%
+
+REM --- Php exe path -------------------------------------------------
+
+REM Path for calling
+set ExePath=e:\wamp64\bin\php\php8.4.5\
+REM ECHO ExePath: "%ExePath%"
+
+if exist "%ExePath%php.exe" (
+    REM path known (WT)
+    ECHO ExePath: "%ExePath%"
+) else (
+    REM Direct call
+    ECHO PHP in path variable
+    set ExePath=
+)
+
+REM "C:\Program Files\php82\php.exe" --version
+"%ExePath%php.exe" --version
+
+ECHO ----------------------------------------------
+ECHO.
+
+echo --- "%ExePath%php.exe" ..\task_http_file\tsk2httpFileCmd.php -p %srcPath% -s %srcFile% -x %dstPath% -y %dstFile%
+"%ExePath%php.exe" ..\task_http_file\tsk2httpFileCmd.php -p %srcPath% -s %srcFile% -x %dstPath% -y %dstFile%
+
+ECHO ----------------------------------------------
+@echo Transform of %srcFile% done
+@echo.
+
+GOTO :EOF
+
+
