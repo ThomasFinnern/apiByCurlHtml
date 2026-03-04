fd3ad9bfd110d43c7e8381dad4ac44d8aa357ed7
ThomasFinnern <ThomasFinnern@users.noreply.github.com>
2026-03-04 17:38:15 +0100

    increased timeout

--- src/curl_tasks/baseCurlTask.php
+++ src/curl_tasks/baseCurlTask.php
@@ -254,7 +254,14 @@ class baseCurlTask extends baseExecuteTasks
         {
 
             $options = [// CURLOPT_URL => sprintf('%s/content/articles/%d', $url, $articleId),
-                CURLOPT_RETURNTRANSFER => true, CURLOPT_ENCODING => 'utf-8', CURLOPT_MAXREDIRS => 10, CURLOPT_TIMEOUT => 30, CURLOPT_FOLLOWLOCATION => true, CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_2TLS, // CURLOPT_CUSTOMREQUEST => 'DELETE',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => 'utf-8',
+                CURLOPT_MAXREDIRS => 10,
+//                CURLOPT_TIMEOUT => 30,
+                CURLOPT_TIMEOUT => 360,  // 3 minutes
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_2TLS,
+                // CURLOPT_CUSTOMREQUEST => 'DELETE',
                 // CURLOPT_HTTPHEADER => $headers,
             ];
 
